6a6257b2108e2e1a2a3bd709f059a5235a305de7
beniciolevy <blevy@udesa.edu.ar>
2025-11-09 22:57:00 -0300

    Corregi la navegacion TAB del app y el homemenu, pase app a componente con estado

--- App.js
+++ App.js
@@ -1,29 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Register from './src/screens/Register';
 import Login from './src/screens/Login';
 import HomeMenu from './src/components/homeMenu';
+import { auth } from './src/firebase/config';
 
 const Stack = createNativeStackNavigator();
 
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    };
+  }
 
-export default function App() {
-  return (
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user: user });
+      } else {
+        this.setState({ user: null });
+      }
+    });
+  }
 
-    <NavigationContainer>
-      <Stack.Navigator>
-
-      
-          <Stack.Screen name="HomeMenu" component={HomeMenu} />
-          
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Register" component={Register} />
-          
+  render() {
+    return (
+      <NavigationContainer>
         
-
-      </Stack.Navigator>
-    </NavigationContainer>
-    
-  );
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          { this.state.user ? (
+            <Stack.Screen name="HomeMenu" component={HomeMenu} />
+          ) : (
+            <>
+              <Stack.Screen name="Login" component={Login} />
+              <Stack.Screen name="Register" component={Register} />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
--- src/components/homeMenu.js
+++ src/components/homeMenu.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-import Login from '../screens/Login';
-import Register from '../screens/Register';
 import NewPost from '../screens/NewPost';
 import Profile from '../screens/Profile';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 const Tab = createBottomTabNavigator();
 
 export default function HomeMenu() {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Home" component={Home} options={ { tabBarIcon: () => <FontAwesome5 name="warehouse" size={24} color="black" /> }} />
-      <Tab.Screen name="NewPost" component={NewPost} options={{ tabBarIcon: () => <FontAwesome5 name="warehouse" size={24} color="black" /> }} />
-      <Tab.Screen name="Profile" component={Profile} options={{ tabBarIcon: () => <FontAwesome5 name="user" size={24} color="black" /> }} />
-      <Tab.Screen name="Login" component={Login} options={ { tabBarIcon: () => <FontAwesome5 name="warehouse" size={24} color="black" /> }} />
-      <Tab.Screen name="Register" component={Register} options={ { tabBarIcon: () => <FontAwesome5 name="warehouse" size={24} color="black" /> }} />
+      <Tab.Screen name="Home" component={Home} options={{ tabBarIcon: () => <FontAwesome5 name="home" size={24} /> }} />
+      <Tab.Screen name="NewPost" component={NewPost} options={{ tabBarIcon: () => <FontAwesome5 name="plus" size={24} /> }} />
+      <Tab.Screen name="Profile" component={Profile} options={{ tabBarIcon: () => <FontAwesome5 name="user" size={24} /> }} />
     </Tab.Navigator>
-
   );
 }
-
-
